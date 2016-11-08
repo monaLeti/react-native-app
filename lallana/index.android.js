fa23978c486b3e6cf0f019568f588e316699eb72
Leticia Trinidad Valderas Rodríguez <ltv@anthillagency.com>
2016-11-08 19:51:08 +0100

    front-end

---
+++ lallana/index.android.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import App from './App'
+
+AppRegistry.registerComponent('lallana', () => App);
